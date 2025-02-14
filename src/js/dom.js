cc590e04e3f135c2edd6ec60e9319d288cbbb87d
cordobag <cordobag23@gmail.com>
2025-02-14 16:38:01 -0500

    Classlist DOM

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,8 +1,8 @@
 const elementos = document.getElementsByClassName("card");
 console.log(elementos);
 
-const elemento = document.getElementById("card1");
-console.log(elemento);
+//const elemento = document.getElementById("card1");
+//console.log(elemento);
 
 const elementoq = document.querySelector("#idinput");
 console.log(elementoq);
@@ -16,3 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     document.querySelector("#btninsertar").addEventListener("click", llenartexto);
 });
+
+
+//cards
+
+const card3 = document.querySelector(".card-3");
+console.log(card3.classList);
+card3.classList.add("ananida"); //añado una clase a mi elemento card-3 guardado en mi variable card3
+            // .remove   elimina una clase
+            // .toogle si tiene la clase q le enviamos, la elimina
+            // SINO la tiene la crea y asi por cada click
+
+card3.addEventListener(
+    "click", ()=>{card3.classList.toggle("red")}
+);
